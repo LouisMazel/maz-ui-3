9e5d29989ee58bd4ed51a27c786bfca28f342772
LouisMazel <mazuel.loic@gmail.com>
2022-01-19 15:22:15 +0100

    docs(docs): adding documentation about theming

--- packages/docs/docs/.vuepress/clientAppEnhance.ts
+++ packages/docs/docs/.vuepress/clientAppEnhance.ts
@@ -1,7 +1,7 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import * as components from 'maz-ui'
-import 'maz-ui/dist/main.css'
-import 'maz-ui/config/tailwindcss/css/_subs/colors-vars.css'
+import 'maz-ui/main.css'
+import 'maz-ui/colors-vars.css'
 
 export default defineClientAppEnhance(({ app }) => {
   Object.entries(components).forEach(([componentName, component]) => {
