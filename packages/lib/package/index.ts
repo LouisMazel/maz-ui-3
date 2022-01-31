ff345701585a279c897ea5da5a50a20a9d5c9c76
LouisMazel <mazuel.loic@gmail.com>
2022-01-31 14:46:34 +0100

    chore(lib): build config

--- packages/lib/package/index.ts
+++ packages/lib/package/index.ts
@@ -1,21 +1,8 @@
-// import { Plugin } from 'vue'
-
 // Import vue components
-// import * as components from './components'
-
-// const plugin: Plugin = {
-//   install: (app) => {
-//     Object.entries(components).forEach(([componentName, component]) => {
-//       app.component(componentName, component)
-//     })
-//   },
-// }
-
-// export default plugin
+import * as components from './components'
 
 export * from './utils'
-// export * from './components'
-// export * from './components/types'
+export * from './components'
 export * from './directives'
 export * from './filters'
-// export { components }
+export { components }
