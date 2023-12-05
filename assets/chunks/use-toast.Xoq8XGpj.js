2025072229ba819727ac116d4a056103c4d5e8e3
LouisMazel <LouisMazel@users.noreply.github.com>
2023-12-05 22:59:46 +0000

    deploy: LouisMazel/maz-ui@4f4f59eb9e03ea3534763bca5bb73d531cb3f546

---
+++ assets/chunks/use-toast.Xoq8XGpj.js
@@ -0,0 +1,1 @@
+import{i as t}from"./inject-strict.woLLb5gm.js";const r=()=>t("toast");export{r as u};
--- assets/chunks/use-wait.fcAUeBP4.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./inject-strict.fupi6q_q.js";const r=()=>t("wait");export{r as u};
